dc9d4eb90e4f2f3b25c85ad76e3e38f14d7aab35
"burgerjang" <"rozen8831@gmail.com">
2020-01-20 16:18:03 +0900

    One more time

--- manifests/run.sh
+++ manifests/run.sh
@@ -3,3 +3,7 @@ kubectl apply -f mysql-db-deployment.yaml -ndefault
 kubectl apply -f petclinic-service.yaml -ndefault
 kubectl apply -f petclinic-deployment.yaml -ndefault
 kubectl apply -f petclinic-ing.yaml -ndefault
+
+# host container log mount
+mkdir -p /logs
+ln -s /var/log/containers/petclinic* /logs/app.log
