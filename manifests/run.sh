037d8b9c0df2473e2b80423b1ae77e10f6de6c57
"burgerjang" <"rozen8831@gmail.com">
2020-01-20 16:53:04 +0900

    Update run.sh for add petclinic.yaml

--- manifests/run.sh
+++ manifests/run.sh
@@ -1,10 +1,16 @@
-kubectl apply -f mysql-db-service.yaml -ndefault
-kubectl apply -f mysql-db-deployment.yaml -ndefault
-kubectl apply -f petclinic-service.yaml -ndefault
-kubectl apply -f petclinic-deployment.yaml -ndefault
-kubectl apply -f petclinic-ing.yaml -ndefault
+#!/bin/bash
 
-# host container log mount
+## Test
+# kubectl apply -f mysql-db-service.yaml -ndefault
+# kubectl apply -f mysql-db-deployment.yaml -ndefault
+# kubectl apply -f petclinic-service.yaml -ndefault
+# kubectl apply -f petclinic-deployment.yaml -ndefault
+# kubectl apply -f petclinic-ing.yaml -ndefault
+
+## All run
+kubectl apply -f petclinic.yaml -ndefault
+
+## host container log mount
 rm -f /logs/app.log
 mkdir -p /logs
 ln -s /var/log/containers/petclinic* /logs/app.log
